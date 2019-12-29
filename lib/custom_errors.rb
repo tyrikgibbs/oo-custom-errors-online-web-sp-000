8040e69de960d717ffe1f66b78ef3d50c158f8c9
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-29 02:07:10 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,15 +1,21 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
-
+ 
+  class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
