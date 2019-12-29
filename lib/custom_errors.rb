210b906c71a53a1cc2c00b905eaf13d073c0e951
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-29 02:08:43 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -15,7 +15,11 @@ class Person
   end
 
   class PartnerError < StandardError
+  def message
+    "you must give the get_married method an argument of an instance of the person class!"
   end
+  end
+
 end
 
 beyonce = Person.new("Beyonce")
